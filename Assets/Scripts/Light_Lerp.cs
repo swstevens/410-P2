d0671d2983d4e2cbd27d33c52b7a29ba50a157d9
tclendo <33107696+tclendo@users.noreply.github.com>
2020-04-14 13:44:26 -0700

    particle add, lerp add

---
+++ Assets/Scripts/Light_Lerp.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light_Lerp : MonoBehaviour {
+
+	public Transform m_Target;
+	public Transform m_Target_2;
+
+	public float speed;
+
+	Vector3 starting_pos;
+	Vector3 target_pos;
+
+	private float startTime;
+	private float journeyLength;
+	
+    void Start() {
+
+    	target_pos = m_Target.position;
+    	starting_pos = m_Target_2.position;
+
+        startTime = Time.time;
+
+        journeyLength = Vector3.Distance(m_Target_2.position, m_Target.position);
+
+    }
+
+    void Update() {
+
+    	float distCovered = (Time.time - startTime) * speed;
+    	float fractionOfJourney = distCovered / journeyLength;
+
+    	transform.position = Vector3.Lerp(starting_pos, target_pos, fractionOfJourney);
+
+        if (transform.position == target_pos) {
+
+        	if (target_pos == m_Target.position) {
+
+        		target_pos = m_Target_2.position;
+        		starting_pos = m_Target.position;
+
+        	} else {
+
+        		target_pos = m_Target.position;
+        		starting_pos = m_Target_2.position;
+
+        	}
+
+        	startTime = Time.time;
+        } 
+    }
+}
