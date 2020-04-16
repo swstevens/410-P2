0ead6546da7c61970330f883f1f3400caf2c0cc9
swstevens <sstevensw@gmail.com>
2020-04-15 20:49:23 -0700

    added more dot product usage

--- Assets/Scripts/Observer.cs
+++ Assets/Scripts/Observer.cs
@@ -6,6 +6,7 @@ public class Observer : MonoBehaviour {
 
 	public Transform player;
 	public GameEnding gameEnding;
+	public float cutoff;
 
 	bool m_IsPlayerInRange;
 
@@ -39,6 +40,25 @@ public class Observer : MonoBehaviour {
 				if(raycastHit.collider.transform == player) {
 
 					gameEnding.CaughtPlayer();
+				}
+			}
+		}
+		// ghost looks at the player if it has LOS and the player is in front of it
+		float cosAngle = Vector3.Dot((player.position - this.transform.position).normalized, this.transform.forward);
+		float angle = Mathf.Acos(cosAngle) * Mathf.Rad2Deg;
+		if (angle < cutoff) 
+		{
+			Vector3 direction = player.position - transform.position + Vector3.up;
+			
+			Ray ray = new Ray (transform.position, direction); 
+			RaycastHit raycastHit;
+
+			if(Physics.Raycast(ray, out raycastHit)) {
+
+				if(raycastHit.collider.transform == player) {
+
+					this.transform.parent.LookAt(player);
+					this.transform.LookAt(player);
 				}
 			}
 		}
